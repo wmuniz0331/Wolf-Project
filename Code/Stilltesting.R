0e1d855e6cea8890f2e89833073b34487837b055
Zach Muniz <munizw@oregonstate.edu>
2025-02-10 10:50:13 -0800

    Random code I am still testing

--- Code/Stilltesting.R
+++ Code/Stilltesting.R
@@ -1,2 +1,8 @@
 #Another Test
 Y = 2+2
+# Generate a random dataset
+set.seed(42)  # For reproducibility
+data <- data.frame(ID = 1:10,Age = sample(20:60, 10, replace = TRUE),
+  Height = round(rnorm(10, mean = 170, sd = 10), 1),
+  Weight = round(rnorm(10, mean = 70, sd = 15), 1))
+View(data)
